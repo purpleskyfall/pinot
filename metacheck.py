575a10c5d37b250bc02b8c368106b74d9a1789d0
purpleskyfall <jiangyingming@live.com>
2017-05-06 21:05:35 +0800

    modify code to suit PEP8

--- metacheck.py
+++ metacheck.py
@@ -1,177 +1,181 @@
 #coding=UTF-8
-#This is a script for checking data information.
 #create date: 2016/12/9
-#last modify: 2016/12/11
 #creater: Zhou Maosheng
 #modifier: Jon Jiang
 #Python version: 3.4
 
-"""This is a script for checking site information"""
+"""Check if site information changed"""
 
 import os
 import re
+import sys
 import glob
-import math
+import yaml
+import argparse
 
-#判别文件名是否为 RINEX 格式 O-文件或 D-文件
+# test if a file is RINEX observation file or Compact RINEX
 RINEXREG = re.compile(r'^[a-z0-9]{4}\d{3}.*\.\d{2}[od]$', re.I)
-#站点信息文件键与观测文件标志对应表
+# mapping of information to RINEX flag
 SITEINFO = {'receiver': {'flag': 'REC # / TYPE / VERS', 'start': 20, 'end': 40}\
             , 'antenna': {'flag': 'ANT # / TYPE', 'start': 20, 'end': 40}\
             , 'delta': {'flag': 'ANTENNA: DELTA H/E/N', 'start': 0, 'end': 42}\
             , 'position': {'flag': 'APPROX POSITION XYZ', 'start': 0, 'end': 42}}
-#保存找不到的站点
+# save the site not found in configuration file
 MISSINGSITES = []
 
-#比较站点信息与RINEX文件
-#info: 站点信息, file: 观测文件
-def compareinfo(info, file, threshold, outtype):
-    """Compare site info and rinex file"""
-    #header用于保持文件头信息，differences用于保持不一致处
+
+# info: site information, file: RINEX file
+# threshold: threshold of position change
+# out_type: list or table
+def compareinfo(info, file, threshold, out_type):
+    """Compare site information and RINEX file"""
+
     header, differences = [], []
     filename = os.path.basename(file)
-    ofile = open(file)
-    #加载文件头信息
-    while True:
-        line = ofile.readline()
-        #查找到文件头结束时跳出
-        if -1 != line.find('END OF HEADER'):
-            break
-        header.append(line)
-    #关闭文件
-    ofile.close()
-    #检查信息
+    # load RINEX file header
+    with open(file) as ofile:
+        for line in ofile:
+            header.append(line)
+            if -1 != line.find('END OF HEADER'):
+                break
+    # start information check
     for item in info:
-        #若配置中的项目不在 SITEINFO 中，直接处理下一项
+        # if a information item is not in SITEINFO, skip it
         if item not in SITEINFO:
             continue
         for line in header:
-            #若该行中未找到要检查的信息的标志，直接检查下一行
+            # if can not find flag in this line, continue to check next line
             if -1 == line.find(SITEINFO[item]['flag']):
                 continue
-            #从文件中按照 SITEINFO 中的配置找出所需的文本串
+            # get information from RINEX header
             fileinfostr = line[SITEINFO[item]['start']: SITEINFO[item]['end']]
-            #如果比较项目为先验坐标
+            # if the checking item is approx position
             if item == 'position':
-                #分割配置信息和观测文件中的先验坐标
+                # get position from configuration file and RINEX file
                 cfgposis = info[item].split()
                 fileposis = fileinfostr.split()
-                #遍历对配置和观测文件中的先验坐标进行比较
+                # check position
                 for (cfgposi, fileposi) in zip(cfgposis, fileposis):
-                    #如果任何一个坐标分量的差异超出了阈值，就应作为结果输出
-                    if threshold < math.fabs(float(cfgposi) - float(fileposi)):
-                        if outtype == 'list' or outtype == 'l':
+                    # if any difference is large than threshold, it should be outputed
+                    if threshold < abs(float(cfgposi) - float(fileposi)):
+                        if out_type == 'list' or out_type == 'l':
                             differences.append(item + ' in cfg file: ' + ', '.join(cfgposis))
                             differences.append(item + ' in obs file: ' + ', '.join(fileposis))
                         else:
-                            message = file.center(40, ' ') + item.center(14, ' ') \
-                                    + (','.join(cfgposis)).center(46, ' ') \
-                                    + (','.join(fileposis)).center(46, ' ')
+                            message = (file.center(40, ' ') + item.center(14, ' ') +
+                                        (','.join(cfgposis)).center(46, ' ') +
+                                        (','.join(fileposis)).center(46, ' '))
                             differences.append(message)
                         break
                 break
-                #其它比较类型
+            # check other information
             if info[item] != fileinfostr.strip():
-                if outtype == 'list' or outtype == 'l':
+                if out_type == 'list' or out_type == 'l':
                     differences.append(item + ' in cfg file: ' + info[item])
-                    differences.append(item + ' in obs file: ' \
-                                + fileinfostr.strip())
+                    differences.append(item + ' in obs file: ' + fileinfostr.strip())
                 else:
-                    message = filename.center(16, ' ') + item.center(14, ' ') \
-                            + info[item].center(46, ' ') + fileinfostr.strip().center(46, ' ')
+                    message = (filename.center(16, ' ') + item.center(14, ' ') + 
+                                info[item].center(46, ' ') +
+                                fileinfostr.strip().center(46, ' '))
                     differences.append(message)
                 break
-    #检查是否有不同
+    # output differences
     if len(differences):
-        if outtype == 'list' or outtype == 'l':
-            print('\n' + file + ' has differences:')
+        if out_type == 'list' or out_type == 'l':
+            print('\n%s has differences:' %file)
         print('\n'.join(differences))
 
-#检查输入文件夹中的与通配符匹配的文件
-#globstr: 通配符, srcpath: 输入文件夹, sitesinfo: 站点信息表, outtype: 消息输出类型, threshold: 先验坐标阈值, recursive: 是否递归
-def checksite(globstr, srcpath, sitesinfo, outtype, threshold, recursive):
+
+# src_dir: source directory, glob_str: glob string,
+# sitesinfo: sitesinfo in configuration file, 
+# threshold: threshold of position difference
+# out_type: list or table, recursive: search file recursively
+def checksite(glob_str, src_dir, sitesinfo, out_type, threshold, recursive):
     """Check site info, search children folder in recursive is set"""
-    #对符合通配符的文件进行遍历
-    for file in glob.glob(os.path.join(srcpath, globstr)):
+
+    for file in glob.glob(os.path.join(src_dir, glob_str)):
         filename = os.path.basename(file)
-        #忽略非RINEX文件
+        # if a file is not RINEX, skip it
         if not RINEXREG.match(filename):
             continue
+        # git file info in configuration file
         site = filename[0:4].lower()
-        #获取站点信息
         info = sitesinfo.get(site)
-        #若站点信息为空，将该站点保存在缺失站点数组中
+        # if a site information not found in configuration file
         if info is None:
             if site not in MISSINGSITES:
                 MISSINGSITES.append(site)
             continue
-        compareinfo(info, file, threshold, outtype)
-    #若用户设置了递归，则继续搜索子文件夹
-    if recursive:
-        for child in os.listdir(srcpath):
-            childpath = os.path.join(srcpath, child)
-            if os.path.isdir(childpath):
-                checksite(globstr, childpath, sitesinfo, outtype, threshold, recursive)
+        compareinfo(info, file, threshold, out_type)
 
-#主函数
-#args: 输入参数
+    # process subfolders if --recursive is setted
+    if recursive:
+        for child in os.listdir(src_dir):
+            childpath = os.path.join(src_dir, child)
+            if os.path.isdir(childpath):
+                checksite(glob_str, childpath, sitesinfo, out_type, threshold, recursive)
+
+
+# args: user input arguments
 def main(args):
     """Main function"""
-    #引入 PyYAML 模块
-    import yaml
-    #检查配置文件是否存在
-    if not os.path.exists(args.cfg):
-        print("Error! Can't find config file: " + args.cfg + ", use -cfg <config> input it!")
-        return 1
-    #加载配置文件
-    cfgfile = open(args.cfg)
-    sitesinfo = yaml.load(cfgfile) #加载用户配置信息
-    srcdirs, globstr, outtype, threshold = args.dir, args.glob, args.out, args.thd
-    #输出提示信息
-    print('----------------- input params ---------------')
-    print('source dirs: ' + srcdirs)
-    print('file mode: ' + globstr)
-    print('config file: ' + args.cfg)
-    print('----------------------------------------------')
-    #若输出形式为表格，则首先打印一个表头
-    if outtype == 'table' or outtype == 't':
-        print('\n' + 'file'.center(16, ' ') + 'type'.center(14, ' ') + 'cfgfile'.center(46, ' ') \
-                + 'obsfile'.center(46, ' '))
-    #开始检查
-    for srcdir in glob.glob(srcdirs):
-        checksite(globstr, srcdir, sitesinfo, outtype, threshold, args.recursive)
-    #输出配置文件中找不到的站点
-    if len(MISSINGSITES) > 0:
-        print('\n' + 'sites not found in cfg file: ' + ', '.join(MISSINGSITES) + '.')
 
+    # valid the configuration file
+    if not os.path.exists(args.cfg):
+        print("Error! Can't find config file: %s" %args.cfg, file=sys.stderr)
+        return 1
+    # load sites information from configuration file
+    with open(args.cfg) as cfgfile:
+        sitesinfo = yaml.load(cfgfile)
+
+    src_dir, glob_str, out_type, threshold = args.dir, args.glob, args.out, args.thd
+    
+    print('---------------------- input params ----------------------')
+    print('source dirs: %s' %src_dir)
+    print('file mode: %s' %glob_str)
+    print('config file: %s' %args.cfg)
+    print('----------------------------------------------------------')
+
+    # if out_type is table, print a table header first
+    if out_type == 'table' or out_type == 't':
+        print('\n' + 'file'.center(16, ' ') + 'type'.center(14, ' ')
+                + 'cfgfile'.center(46, ' ') + 'obsfile'.center(46, ' '))
+    
+    for directory in glob.glob(src_dir):
+        checksite(glob_str, directory, sitesinfo, out_type, threshold, args.recursive)
+    
+    if len(MISSINGSITES) > 0:
+        print('\nsites not found in cfg file: %s.' %', '.join(MISSINGSITES))
+    
     return 0
+
 
 #脚本初始化方法，解析用户的输入参数
 def init_args():
-    """Initilize function"""
-    #引入 argparse 模块用于解析用户输入
-    import argparse
-    #创建解析器
-    parser = argparse.ArgumentParser(description=\
-                        "compare rinex obs files' meta info from YAML configuration.")
-    #添加所需参数信息
-    parser.add_argument('-r', '--recursive', action='store_true'\
-                        , help='search file in child folder')
-    parser.add_argument('-v', '--version', action='version', version='metacheck.py 0.3.9')
-    parser.add_argument('-dir', metavar='<input_dir>', default='./'\
-                        , help='input dir mode [default: current]')
-    parser.add_argument('-glob', metavar='<mode>', default='*.[0-9][0-9][oOdD]'\
-                        , help='mode is filename search mode [default: *.[0-9][0-9][oOdD]')
-    parser.add_argument('-cfg', metavar='<config>', default='_sitesinfo.yml'\
-                        , help='configuration YAML file [default: ./_sitesinfo.yml]')
-    parser.add_argument('-out', metavar='<type>', default='list'\
-                        , choices=['list', 'l', 'table', 't']\
-                        , help='format of output messages, list or table [default: list]')
-    parser.add_argument('-thd', metavar='<threshold>', default=10, type=int\
-                        , help='threshold of position change [default: 10]')
+    """Initilize function, parse user input"""
 
-    #运行主程序方法
+    # initilize a argument parser
+    parser = argparse.ArgumentParser(
+        description="compare RINEX files' meta between YAML configuration.")
+    # add arguments
+    parser.add_argument('-v', '--version', action='version',
+                        version='metacheck.py 0.3.10')
+    parser.add_argument('-r', '--recursive', action='store_true',
+                        help='search file in subfolders')
+    parser.add_argument('-dir', metavar='<input_dir>', default='.',
+                        help='input dir mode [default: current]')
+    parser.add_argument('-glob', metavar='<mode>', default='*.[0-9][0-9][oOdD]',
+                        help='filename search mode [default: *.[0-9][0-9][oOdD]')
+    parser.add_argument('-cfg', metavar='<config>', default='_sitesinfo.yml',
+                        help='configuration YAML file [default: _sitesinfo.yml]')
+    parser.add_argument('-out', metavar='<type>', default='list',
+                        choices=['list', 'l', 'table', 't'],
+                        help='format of output messages, list or table [default: list]')
+    parser.add_argument('-thd', metavar='<threshold>', default=10, type=int,
+                        help='threshold of position change [default: 10]')
+
     return main(parser.parse_args())
+
 
 if __name__ == '__main__':
     init_args()
--- unificate.py
+++ unificate.py
@@ -1,60 +1,60 @@
 #coding=UTF-8
-#this script is used to unificate rinex files by a config.yml file.
 #create date: 2016/12/5
 #creater: Zhou Maosheng
-#modifer: Jon Jiang
 #Python version: 3.4
 
 """Unificate rinex files by a config.yml file"""
 
-#引入所需模块
 import os
-import glob
 import re
+import sys
+import glob
+import time
+import yaml
+import argparse
 import threading
 
-#正则表达式，测试文件名是否为 RINEX
+# test if a filename is RINEX
 RINEXREG = re.compile(r'^[a-z0-9]{4}\d{3}.*\.\d{2}[o]$', re.I)
-#正则表达式，测试字符串中是否包含字母
+# test if a string contains alpha letters
 ALPHAREG = re.compile(r'[a-z]+\s*', re.I)
-#站点信息配置项与TEQC参数对应表
+# mapping of sites info and TEQC param
 TEQCITEM = {'observer': '-O.o', 'agency': '-O.ag', 'rm_sys': '', 'obs_type': '-O.obs',
             'interval': '-O.dec', 'receiver': '-O.rt', 'antenna': '-O.at', 'delta': '-O.pe',
             'position': '-O.px'}
-#同时执行的线程数
+# params for multithread
 THREADSIZE = 6
-#正在执行的线程数
 THREADCOUNT = 0
-#线程锁定标志
 MUTEXCOUNT = threading.Lock()
 
-#若文件夹不存在则创建
-#dirpath: 文件夹路径
-def createdir(dirpath):
-    """Create new directory if not exist"""
-    if not os.path.exists(dirpath):
-        print('make dir: ' + dirpath)
-        os.mkdir(dirpath)
 
-#返回配置文件中某个站点的信息，若没有该站点，返回 all 中的信息作为默认配置
-#site: 站点名; cfgfile: 配置文件
+# dirpath: directory path
+def createdir(dir_path):
+    """Create new directory if not exist"""
+
+    if not os.path.exists(dir_path):
+        print('create directory: %s' %dir_path)
+        os.makedirs(dir_path)
+
+
+# site: site name, sitesinfo: information in configuration file
 def getinfo(site, sitesinfo):
     """Get information from cfgfile"""
-    #获得特定站点的信息
-    site = site.lower()
-    info = sitesinfo.get(site, {})
-    #加载 all 中的信息作为默认配置
+
+    # get special site information from configuration
+    info = sitesinfo.get(site.lower(), {})
+    # get default information for 'all'
     default = {}
     if 'all' in sitesinfo:
         default = sitesinfo['all'].copy()
-    #对于重复的属性，使用站点中的项目
+    # use site information to overwrite default
     for key in info:
         default[key] = info[key]
 
     return default
 
-#将用户配置信息转化为对应的TEQC参数
-#info: 站点配置信息
+
+# info: site information
 def getparams(info):
     """Get teqc params from user config info"""
     params = ''
@@ -65,7 +65,6 @@ def getparams(info):
         elif key == 'obs_type':
             params += ' ' + TEQCITEM[key] + ' ' + ','.join(info[key])
         else:
-            #如果站点信息中包含字母，说明该项是字符串，需要用双引号包裹
             if ALPHAREG.match(str(info[key])):
                 params += ' ' + TEQCITEM[key] + ' "' + str(info[key]) + '"'
             else:
@@ -73,122 +72,131 @@ def getparams(info):
 
     return params
 
-#使用 os.system() 方法执行 TEQC 程序进行标准化
-#参数表：file: 文件及路径, params: teqc 参数, outputpath: 输出路径
-def teqc(file, params, outputpath):
-    """execute system command by os.system()"""
+
+# file: file path, params: teqc params, out_dir: output directory
+def teqc(file, params, out_dir):
+    """Execute TEQC command by os.system()"""
+
     global THREADCOUNT
     filename = os.path.basename(file)
-    os.system('teqc' + params + ' ' + file + ' > ' + os.path.join(outputpath, filename)) #执行TEQC
-    #TEQC 程序执行完毕之后，将正在执行线程数减1
+    os.system('teqc' + params + ' ' + file + ' > ' + os.path.join(out_dir, filename))
+    # decrease the THREADCOUNT when finished
     if MUTEXCOUNT.acquire():
         THREADCOUNT -= 1
         MUTEXCOUNT.release()
 
-#采用异步多线程的方式执行命令
-#参数表：commands: 待执行命令列表
+
+# commands: commands needed to be executed
 def runtaskasync(commands):
-    """execute commands async"""
+    """Run commands async"""
+
     global THREADCOUNT
     thread = None
+
     while len(commands) > 0:
-        #当前运行的线程数小于 THREADSIZE 时，获得对 THREADCOUNT 的锁定，创建新线程
+        # create new thread when THREADCOUNT less than THREADSIZE
         if THREADCOUNT < THREADSIZE and MUTEXCOUNT.acquire():
             #从待执行命令列表中取出一个执行
             cmd = commands.pop()
-            print('unificate ' + cmd['file'] + ' ......')
+            print('unificate %s ......' %cmd['file'])
             thread = threading.Thread(target=teqc, args=(cmd['file'], cmd['params'], cmd['output']))
             thread.setDaemon(True)
             thread.start()
-            #线程数增1
+            # increase the THREADCOUNT
             THREADCOUNT += 1
-            #解除对 THREADCOUNT 的锁定
             MUTEXCOUNT.release()
+
     #若待执行命令已经为空，则等待所有线程执行完毕之后退出
+    # when commands are empty, wait all executing task are finished
     if thread is not None:
         thread.join()
 
-#对输入文件夹内所有符合通配符的文件进行处理
-#globstr: 通配符; srcpath: 源文件夹路径; outputpath: 输出文件夹; sitesinfo: 用户配置信息; recursive: 是否递归搜索
-def process(globstr, srcpath, outputpath, sitesinfo, recursive):
+
+# src_dir: source directory, out_dir: output directory,
+# glob_str: glob string, sitesinfo: information in configuration file
+# recursive: search file recursively
+def process(glob_str, src_dir, out_dir, sitesinfo, recursive):
     """Unificate rinex files by user config info"""
-    commands = [] #用于保存所需执行的teqc命令的数组
-    #遍历与通配符匹配的文件，将需执行的命令加入 commands
-    for file in glob.glob(os.path.join(srcpath, globstr)):
+
+    commands = []
+    for file in glob.glob(os.path.join(src_dir, glob_str)):
         filename = os.path.basename(file)
-        #忽略非RINEX文件
+        # if a file is not RINEX, skip it
         if not RINEXREG.match(filename):
             continue
-        #从文件名中获得4字符站点名，并查找站点信息
-        info = getinfo(filename[0:4], sitesinfo) #获得站点信息
-        params = getparams(info) #获得对应于站点信息的TEQC命令参数
-        commands.append({'file': file, 'params': params, 'output': outputpath})
-    #以多线程的方式执行 commands 中的命令
+        # get site name and information
+        info = getinfo(filename[0:4], sitesinfo)
+        params = getparams(info)
+        commands.append({'file': file, 'params': params, 'output': out_dir})
+    
+    # run commands async
     runtaskasync(commands[::-1])
-    #若用户设置了-f参数，则递归处理子文件夹的内容
+    # process subfolders if --recursive is setted
     if recursive:
-        for child in os.listdir(srcpath):
-            childpath = os.path.join(srcpath, child)
-            if os.path.isdir(childpath):
-                process(globstr, childpath, outputpath, sitesinfo, recursive)
+        for child in os.listdir(src_dir):
+            child_path = os.path.join(src_dir, child)
+            if os.path.isdir(child_path):
+                process(glob_str, child_path, out_dir, sitesinfo, recursive)
 
-#主函数
-#args: 命令行参数表
+
+# args: user input arguments
 def main(args):
     """Main function"""
-    import time
-    import yaml
-    #检查配置文件是否存在
+
+    # valid configuration file
     if not os.path.exists(args.cfg):
-        print("Error! Can't find config file: " + args.cfg + ", use -cfg <config> input it!")
+        print("Error! Can't find config file: %s" %args.cfg, file=sys.stderr)
         return 1
-    #解析用户的 YAML 配置文件
-    cfgfile = open(args.cfg)
-    sitesinfo = yaml.load(cfgfile) #加载用户配置
-    cfgfile.close()
-    srcdirs, globstr, outputdir = args.dir, args.glob, args.out
-    #记录开始时间
+    
+    # load configuration file
+    with open(args.cfg) as cfgfile:
+        sitesinfo = yaml.load(cfgfile)
+
+    src_dir, glob_str, out_dir = args.dir, args.glob, args.out
+    createdir(out_dir)
+    
+    print('---------------------- input params ------------------------')
+    print('source dirs: %s' %src_dir)
+    print('output dir: %s' %out_dir)
+    print('file mode: %s' %glob_str)
+    print('config file: %s' %args.cfg)
+    print('------------------------------------------------------------\n')
+
+    # save start time
     start = time.clock()
-    #输出提示信息
-    print('-------------------- input params ------------------------')
-    print('source dirs: ' + srcdirs)
-    print('file mode: ' + globstr)
-    print('output dir: ' + outputdir)
-    print('config file: ' + args.cfg + '\n')
-    createdir(outputdir) #若输出文件夹不存在，则创建它
-    print('--------------------- start process ----------------------')
-    #进行标准化处理
-    for srcdir in glob.glob(srcdirs):
-        process(globstr, srcdir, outputdir, sitesinfo, args.recursive)
-    #记录结束时间
+    
+    for directory in glob.glob(src_dir):
+        process(glob_str, directory, out_dir, sitesinfo, args.recursive)
+    
+    # calculate running time
     end = time.clock()
-    #计算并输出运行时间
-    print('\nuse seconds: ' + str(end - start))
+    print('\nuse seconds: %.5f' %(end - start))
 
     return 0
 
-#脚本初始化方法，解析用户的输入参数
-def init_args():
-    """Initilize function"""
-    import argparse
-    #创建解析器
-    parser = argparse.ArgumentParser(description=\
-                                    'unificate rinex obs file by a YAML configuration.')
-    #添加所需参数信息
-    parser.add_argument('-r', '--recursive', action='store_true'\
-                        , help='search file in child folder')
-    parser.add_argument('-v', '--version', action='version', version='unificate.py 0.4.4')
-    parser.add_argument('-dir', metavar='<input_dir>', default='./'\
-                        , help='input dir mode [default: current]')
-    parser.add_argument('-glob', metavar='<mode>', default='*.[0-9][0-9][oO]'\
-                        , help='mode is filename search mode [default: *.[0-9][0-9][oO]')
-    parser.add_argument('-out', metavar='<output>', default='unificated'\
-                        , help='output directory [default: unificated in current]')
-    parser.add_argument('-cfg', metavar='<config>', default='_sitesinfo.yml'\
-                        , help='configuration YAML file [default: _sitesinfo.yml]')
 
-    #运行主程序方法
+def init_args():
+    """Initilize function, parse user input"""
+
+    # initilize a argument parser
+    parser = argparse.ArgumentParser(
+        description='unificate RINEX obs file by a YAML configuration.')
+    # add arguments
+    parser.add_argument('-v', '--version', action='version',
+                        version='unificate.py 0.4.5')
+    parser.add_argument('-r', '--recursive', action='store_true',
+                        help='search file in subfolders')
+    parser.add_argument('-dir', metavar='<input_dir>', default='.',
+                        help='input dir mode [default: current]')
+    parser.add_argument('-glob', metavar='<mode>', default='*.[0-9][0-9][oO]',
+                        help='filename search mode [default: *.[0-9][0-9][oO]')
+    parser.add_argument('-out', metavar='<output>', default='unificated',
+                        help='output dir [default: unificated in current]')
+    parser.add_argument('-cfg', metavar='<config>', default='_sitesinfo.yml',
+                        help='configuration YAML file [default: _sitesinfo.yml]')
+
     return main(parser.parse_args())
+
 
 if __name__ == '__main__':
     init_args()
