0217125ccff63c375ec2963e16cd57e9f607de98
Jon Jiang <jiangyingming@live.com>
2017-09-22 15:41:42 +0800

    Rewrite qualitycheck, drop -dir and -glob params, add multi-thread support.

--- qualitycheck.py
+++ qualitycheck.py
@@ -1,219 +1,172 @@
-#coding=UTF-8
-#creater: Jon Jiang
-#datetime: 2016-12-20
-#Python version: 3.4
-#TEQC version: 2016Nov7
+#!/usr/bin/env python3
+# coding=UTF-8
+"""Quality check for RINEX observation files using TEQC software,
+print primary marks of data quality. Using concurent.futures.
 
-"""Check the quality of RINEX observation files by TEQC"""
+The RINEX file quality check function rely on TEQC software, Check if
+you have installed TEQC by typing `teqc -help` in cmd.
 
-import os
-import re
-import glob
+:author: Jon Jiang
+:email: jiangyingming@live.com
+"""
+from concurrent import futures
 import argparse
 import datetime
+import glob
+import itertools
+import os
+import subprocess
+
+MAX_THREADING = max(6, os.cpu_count())
+QUALITYINFO = (
+    {'name': 'start', 'flag': 'Time of start of window :', 'pos': slice(25, 51)},
+    {'name': 'end', 'flag': 'Time of  end  of window :', 'pos': slice(37, 51)},
+    {'name': 'length', 'flag': 'Time line window length :', 'pos': slice(26, 42)},
+    {'name': 'SN1', 'flag': 'Mean S1                 :', 'pos': slice(26, 31)},
+    {'name': 'SN2', 'flag': 'Mean S2                 :', 'pos': slice(26, 31)}
+)
 
 
-# test if a file is RINEX observation file
-RINEXREG = re.compile(r'^[a-z0-9]{4}\d{3}.*\.\d{2}[o]$', re.I)
-# flag of quality message in TEQC output file
-QUALITYINFO = [
-    {'name': 'start', 'flag': 'Time of start of window :', 'start': 38, 'end': None, 'len': 16},
-    {'name': 'end', 'flag': 'Time of  end  of window :', 'start': 38, 'end': None, 'len': 16},
-    {'name': 'length', 'flag': 'Time line window length :', 'start': 26, 'end': 42, 'len': 16},
-    {'name': 'SN1', 'flag': 'Mean S1                 :', 'start': 26, 'end': 31, 'len': 9},
-    {'name': 'SN2', 'flag': 'Mean S2                 :', 'start': 26, 'end': 31, 'len': 9}]
-# flag of MP1, MP2, CSR
-MPCSRFLAG = 'first epoch    last epoch    hrs   dt  #expt  #have   %   mp1   mp2 o/slps'
-# other file type maybe output after quality check
-OTHERFILES = ['.azi', '.ele', '.iod', '.ion', '.mp1', '.mp2', '.sn1', '.sn2']
+def quality_check(src_file):
+    """Run quality check for source file using TEQC software.
+
+    1. If run TEQC successfully, return quality check report;
+    2. If run TEQC failed, return None.
+    """
+    args = 'teqc', '+qc', '-plot', '-rep', src_file
+    status, output = subprocess.getstatusoutput(' '.join(args))
+    # if exit status of TEQC software is not 0, means error
+    if status > 0:
+        return None
+
+    return output.split('\n')
 
 
-def tableheader():
-    """Create a table header for table output mode"""
+def parse_report(report):
+    """Parse TEQC quality check report, return a tuple:
+    (date, start, end, length, SN1, SN2, MP1, MP2, CSR).
 
-    tabheaders = []
+    Example:
 
-    tabheaders.append('file'.center(14, ' '))
-    tabheaders.append('date'.center(12, ' '))
+    >>> report = [
+    ... 'Time of start of window : 2017 Aug 10  00:00:00.000',
+    ... 'Time of  end  of window : 2017 Aug 10  23:59:30.000',
+    ... 'Time line window length : 23.99 hour(s), ticked every ...',
+    ... 'Mean S1                 : 46.95 (sd=5.80 n=49483)',
+    ... 'Mean S2                 : 42.21 (sd=8.18 n=48411)',
+    ... '      first epoch    last epoch    hrs   dt  #expt  #have   %'
+    ... '   mp1   mp2 o/slps',
+    ... 'SUM 17  8 10 00:00 17  8 10 23:59 24.00  30     -   47669  - '
+    ... '  0.43  0.42   3972'
+    ... ]
+    >>> result = parse_report(report) # doctest: +NORMALIZE_WHITESPACE
+    >>> result[0:4]
+    ('2017-08-10', '00:00:00.000', '23:59:30.000', '23.99 hour(s)')
+    >>> [round(num, 2) for num in result[4:]]
+    [46.95, 42.21, 0.43, 0.42, 0.25]
+    """
+    # because the order of output messages are same, so we only need to
+    # check the segment once.
+    marks, last_no = {}, 0
     for item in QUALITYINFO:
-        tabheaders.append(item['name'].center(item['len'], ' '))
-    tabheaders.append('MP1'.center(8, ' '))
-    tabheaders.append('MP2'.center(8, ' '))
-    tabheaders.append('CSR'.center(8, ' '))
+        for number, line in enumerate(report[last_no:], last_no):
+            if item['flag'] not in line:
+                continue
+            marks[item['name']] = line[item['pos']].strip()
+            last_no = number
+            break
+    # restruct the quality marks into a tuple
+    sn1, sn2 = float(marks['SN1']), float(marks['SN2'])
+    date = datetime.datetime.strptime(marks['start'][0:11], '%Y %b %d')
+    start, end = marks['start'][11:].strip(), marks['end']
+    length = marks['length'].split(',')[0]
+    # get MP1, MP2, CSR from the last line of report
+    last_line = report[-1]
+    mp1, mp2 = float(last_line[63:67]), float(last_line[69:73])
+    olps = float(last_line[75:])
+    csr = 1000 / olps
 
-    return ''.join(tabheaders)
-
-
-# filename: name of file
-def getyear(filename):
-    """Get 4 digit year from RINEX filename"""
-
-    year = int(filename[-3:-1])
-    return 2000 + year if year < 80 else 1900 + year
-
-
-# filename: name of file
-def getdoy(filename):
-    """Get day of year from rinex obs filename"""
-
-    doy = filename[4:7]
-    return int(doy)
-
-
-# year: year of observation data, doy: day of year
-def getdate(year, doy):
-    """Convert year and doy to date"""
-
-    initdate = datetime.date(year, 1, 1)
-    delta = datetime.timedelta(doy - 1)
-    result = initdate + delta
+    result = (date.strftime('%Y-%m-%d'), start, end,
+              length, sn1, sn2, mp1, mp2, csr)
 
     return result
 
 
-# result_file: result file of quality check, out_type: list or table
-def getqualitymarks(result_file, out_type):
-    """Return qualitycheck marks from result file, list or table"""
-
-    # output info
-    outputs = []
-    # index of QUALITYINFO
-    infoindex = 0
-
-    with open(result_file) as result_reader:
-        for line in result_reader:
-            # because the order of output messages are same, so we can only check a message flag once
-            if infoindex < len(QUALITYINFO) and line.find(QUALITYINFO[infoindex]['flag']) != -1:
-                # get value of quality check result
-                info = QUALITYINFO[infoindex]
-                value = line[info['start']: info['end']].strip()
-                # when process the length of observation, need to split the output by ','
-                if infoindex == 2 and value.find(',') != -1:
-                    value = value.split(',')[0]
-                if out_type == 'list' or out_type == 'l':
-                    outputs.append(info['name'] + ': ' + value)
-                else:
-                    outputs.append(value.center(info['len'], ' '))
-                # increase the index to check next message
-                infoindex += 1
-                continue
-            # when the check of QUALITYINFO is finished, start to process MP1, MP2 and CSR
-            if infoindex == len(QUALITYINFO) and line.find(MPCSRFLAG) != -1:
-                # messages are in next line
-                line = result_reader.readline()
-                mp1, mp2, o_slps = line[63:67].strip(), line[69:73].strip(), line[75:].strip()
-                # calculate CSR by o_slps
-                csr = round(1000 / float(o_slps), 2)
-                # add to output info
-                if out_type == 'list' or out_type == 'l':
-                    outputs.append('MP1: ' + mp1)
-                    outputs.append('MP2: ' + mp2)
-                    outputs.append('CSR: ' + str(csr))
-                else:
-                    outputs.append(mp1.center(8, ' '))
-                    outputs.append(mp2.center(8, ' '))
-                    outputs.append(str(csr).center(8, ' '))
-                break
-
-    return outputs
-
-
-# file: RINEX observation file, out_type: list or table
-def execteqc(file, out_type):
-    """Execute TEQC quality check"""
-    # get file directory path and filename
-    filedir, filename = os.path.split(file)
-    # get the path of result file
-    result_file = os.path.join(filedir, filename[0:-1] + 'S')
-    outputs = []
-    # run teqc
-    os.system('teqc +out temp.log ++err temp.log +qc ' + file)
-    # get quality check info and remove the result file
-    if os.path.exists(result_file):
-        outputs = getqualitymarks(result_file, out_type)
-        os.remove(result_file)
-    # remove other file maybe create in quality check
-    for file_type in OTHERFILES:
-        tempfile = os.path.join(filedir, filename[0:-4] + file_type)
-        if os.path.exists(tempfile):
-            os.remove(tempfile)
-    # get the observation date of RINEX file
-    dateofdata = getdate(getyear(filename), getdoy(filename))
-    strdate = dateofdata.strftime('%Y-%m-%d')
-    
-    # output the quality check info
-    if out_type == 'list' or out_type == 'l':
-        print('\n%s quality marks:' %file)
-        print('date: %s' %strdate)
-        for output in outputs:
-            print(output)
+def print_marks(marks, out_fmt):
+    """Print marks of quality check, the out_fmt is list or table."""
+    if out_fmt == 'list' or out_fmt == 'l':
+        message = ('\n{0} quality marks:\n' 'date: {1}\n' 'start: {2}\n'
+                   'end: {3}\n' 'length: {4}\n' 'SN1: {5:.2f}\n'
+                   'SN2: {6:.2f}\n' 'MP1: {7:.2f}\n' 'MP2: {8:.2f}\n'
+                   'CSR: {9:.2f}')
+        print(message.format(*marks))
     else:
-        outputs.insert(0, strdate.center(12, ' '))
-        outputs.insert(0, filename.ljust(14, ' '))
-        print(''.join(outputs))
+        message = ('{0: ^14s} {1: ^12s} {2: ^14s} {3: ^14s} {4: ^15s}  '
+                   '{5: 6.2f}  {6: 6.2f}  {7: 6.2f}  {8: 6.2f}  {9: 5.2f}')
+        print(message.format(os.path.basename(marks[0]), *marks[1:]))
 
 
-# src_dir: source directory, glob_str: glob string,
-# out_type: list or table, recursive: search file recursively
-def qualitycheck(src_dir, glob_str, out_type, recursive):
-    """check obs quality by TEQC"""
-    for file in glob.glob(os.path.join(src_dir, glob_str)):
-        filename = os.path.basename(file)
-        #使用正则表达式检查文件名是否为RINEX
-        if not RINEXREG.match(filename):
-            continue
-        #使用TEQC程序进行质量检查处理
-        execteqc(file, out_type)
-    #如果用户设置了递归，则继续处理子文件夹
-    if recursive:
-        for child in os.listdir(src_dir):
-            childpath = os.path.join(src_dir, child)
-            if os.path.isdir(childpath):
-                qualitycheck(childpath, glob_str, out_type, recursive)
+def parallel_teqc(src_files, out_fmt):
+    """Parallel run function using argvs."""
+    with futures.ThreadPoolExecutor(max_workers=MAX_THREADING) as executor:
+        todo_map = {}
+        for src_file in src_files:
+            future = executor.submit(quality_check, src_file)
+            todo_map[future] = src_file
+        task_iter = futures.as_completed(todo_map)
+        failed_files = []
+        for future in task_iter:
+            src_file = todo_map[future]
+            # return None means task is success
+            res = future.result()
+            if res:
+                record = (src_file, *parse_report(res))
+                print_marks(record, out_fmt)
+            else:
+                failed_files.append(os.path.basename(src_file))
+
+        return failed_files
 
 
-# args: user input arguments
 def main(args):
-    """Main function"""
+    """Main function."""
+    globstrs, out_fmt, recursive = args.files, args.out, args.recursive
+    # collect input globstrs into a glob list
+    globs = [glob.iglob(globstr, recursive=recursive) for globstr in globstrs]
+    src_files = [src for src in itertools.chain(*globs)]
+    # make input args for teqc function
+    print('Start processing: {} ...'.format(', '.join(globstrs)))
+    # if output format is table, print a table header first
+    if out_fmt == 'table' or out_fmt == 't':
+        header = ('file', 'date', 'start', 'end', 'length',
+                  'SN1', 'SN2', 'MP1', 'MP2', 'CSR')
+        style = ('\n{0: ^14s} {1: ^12s} {2: ^14s} {3: ^14s} {4: ^15s} '
+                 '{5: >6s}  {6: >6s}  {7: >6s}  {8: >6s}  {9: >5s}')
+        print(style.format(*header))
+    # start parallel processing
+    failed = parallel_teqc(src_files, out_fmt)
+    if failed:
+        print('\nQuality check failed files: {}'.format(', '.join(failed)))
 
-    src_dir, glob_str, out_type = args.dir, args.glob, args.out
-
-    print('----------------- input params ---------------')
-    print('source dir: %s' %src_dir)
-    print('file mode: %s' %glob_str)
-    print('----------------------------------------------')
-
-    # if the out_type is table, print a table header first
-    if out_type == 'table' or out_type == 't':
-        print('\n' + tableheader())
-    # start quality check
-    for srcdir in glob.glob(src_dir):
-        qualitycheck(srcdir, glob_str, out_type, args.recursive)
-    # remove temp file
-    if os.path.exists('temp.log'):
-        os.remove('temp.log')
-    
     return 0
 
 
 def init_args():
     """Initilize function, parse user input"""
-
     # initilize a argument parser
     parser = argparse.ArgumentParser(
-        description="rinex quality check script used TEQC.")
+        description="Quality check for RINEX observation files using TEQC."
+    )
     # add arguments
     parser.add_argument('-v', '--version', action='version',
-                        version='qualitycheck.py 0.2.2')
+                        version='%(prog)s 0.4.0')
     parser.add_argument('-r', '--recursive', action='store_true',
-                        help='search file in subfolders')
-    parser.add_argument('-dir', metavar='<input_dir>', default='./',
-                        help='input dir mode [default: current]')
-    parser.add_argument('-glob', metavar='<mode>', default='*.[0-9][0-9][oO]',
-                        help='filename search mode [default: *.[0-9][0-9][oO]')
-    parser.add_argument('-out', metavar='<type>', default='list',
+                        help='search file recursively')
+    parser.add_argument('-out', metavar='<format>', default='table',
                         choices=['list', 'l', 'table', 't'],
-                        help='format of output messages, list or table [default: list]')
+                        help='output format, list or table [default: table]')
+    parser.add_argument('files', metavar='<file>', nargs='+',
+                        help='file will be processed')
 
     return main(parser.parse_args())
 
